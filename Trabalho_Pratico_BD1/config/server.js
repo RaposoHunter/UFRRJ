5556cf9acbac38076447bca6cb1ae2d1d8ac5a94
Pedro Raposo <55608596+RaposoHunter@users.noreply.github.com>
2020-11-13 19:35:11 -0300

    Update server.js

--- Trabalho_Pratico_BD1/config/server.js
+++ Trabalho_Pratico_BD1/config/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "content-type"); 
-	res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
     next();
 });
